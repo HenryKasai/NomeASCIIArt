f480ed88211d2baae4adb877f72bbacc9b774af6
HenryKasai <henrykasai1775@gmail.com>
2020-06-03 18:08:56 -0300

    Exercício Resolvido.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Figgle;
+
+namespace NomeASCIIArt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Olá usuário! Por favor digite seu nome");
+            string nome = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(FiggleFonts.Fraktur.Render(nome));
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+        }
+    }
+}
